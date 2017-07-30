80babe6965a9179be184ff511f4117199ba1e05c
Your Name <you@example.com>
2017-07-30 21:31:48 +0800

    x\

---
+++ www/assets/tests.js
@@ -0,0 +1,327 @@
+'use strict';
+
+define('fe/tests/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/application/application-comp/component.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/application/application-comp/component.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/application/route.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/application/route.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/components/plain-comp/component.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/components/plain-comp/component.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/home/route.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/home/route.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/index/route.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/index/route.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/login/login-comp/component.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/login/login-comp/component.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/login/route.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/login/route.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/resetpwd/route.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/resetpwd/route.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('pods/signup/route.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'pods/signup/route.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+});
+define('fe/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = destroyApp;
+  function destroyApp(application) {
+    _ember.default.run(application, 'destroy');
+  }
+});
+define('fe/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember', 'fe/tests/helpers/start-app', 'fe/tests/helpers/destroy-app'], function (exports, _qunit, _ember, _startApp, _destroyApp) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  exports.default = function (name) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _startApp.default)();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return resolve(afterEach).then(function () {
+          return (0, _destroyApp.default)(_this.application);
+        });
+      }
+    });
+  };
+
+  var resolve = _ember.default.RSVP.resolve;
+});
+define('fe/tests/helpers/resolver', ['exports', 'fe/resolver', 'fe/config/environment'], function (exports, _resolver, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+
+  var resolver = _resolver.default.create();
+
+  resolver.namespace = {
+    modulePrefix: _environment.default.modulePrefix,
+    podModulePrefix: _environment.default.podModulePrefix
+  };
+
+  exports.default = resolver;
+});
+define('fe/tests/helpers/start-app', ['exports', 'ember', 'fe/app', 'fe/config/environment'], function (exports, _ember, _app, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = startApp;
+  function startApp(attrs) {
+    var attributes = _ember.default.merge({}, _environment.default.APP);
+    attributes = _ember.default.merge(attributes, attrs); // use defaults, but you can override;
+
+    return _ember.default.run(function () {
+      var application = _app.default.create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+      return application;
+    });
+  }
+});
+define('fe/tests/integration/pods/components/plain-comp/component-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForComponent)('plain-comp', 'Integration | Component | plain comp', {
+    integration: true
+  });
+
+  (0, _emberQunit.test)('it renders', function (assert) {
+
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+
+    this.render(Ember.HTMLBars.template({
+      "id": "oo3jJRIW",
+      "block": "{\"statements\":[[1,[26,[\"plain-comp\"]],false]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), '');
+
+    // Template block usage:
+    this.render(Ember.HTMLBars.template({
+      "id": "ECL3qwMP",
+      "block": "{\"statements\":[[0,\"\\n\"],[6,[\"plain-comp\"],null,null,{\"statements\":[[0,\"      template block text\\n\"]],\"locals\":[]},null],[0,\"  \"]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), 'template block text');
+  });
+});
+define('fe/tests/test-helper', ['fe/tests/helpers/resolver', 'ember-qunit', 'ember-cli-qunit'], function (_resolver, _emberQunit, _emberCliQunit) {
+  'use strict';
+
+  (0, _emberQunit.setResolver)(_resolver.default);
+  (0, _emberCliQunit.start)();
+});
+define('fe/tests/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('helpers/destroy-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/module-for-acceptance.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/start-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('integration/pods/components/plain-comp/component-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/pods/components/plain-comp/component-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/pods/application/route-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/pods/application/route-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/pods/home/route-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/pods/home/route-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/pods/index/route-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/pods/index/route-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/pods/login/route-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/pods/login/route-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/pods/resetpwd/route-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/pods/resetpwd/route-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/pods/signup/route-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/pods/signup/route-test.js should pass ESLint\n\n');
+  });
+});
+define('fe/tests/unit/pods/application/route-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:application', 'Unit | Route | application', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('fe/tests/unit/pods/home/route-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:home', 'Unit | Route | home', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('fe/tests/unit/pods/index/route-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:index', 'Unit | Route | index', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('fe/tests/unit/pods/login/route-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:login', 'Unit | Route | login', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('fe/tests/unit/pods/resetpwd/route-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:resetpwd', 'Unit | Route | resetpwd', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('fe/tests/unit/pods/signup/route-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:signup', 'Unit | Route | signup', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+require('fe/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
