55cee996fc7ae4001c1013b16396c19e5548b1be
Your Name <you@example.com>
2017-08-02 02:17:06 +0800

    x

--- fe/app/pods/components/show-protocol/component.js
+++ fe/app/pods/components/show-protocol/component.js
@@ -1,28 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  geo: Ember.inject.service(),
-  init() {
-    this._super(...arguments);
-    let geo = this.get('geo');
-    let i = 1;
-    alert('init')
-    let watchID = geo.watch(pos => {
-      alert('watchSuccess');
-      this.set('position', pos);
-      alert(pos.coords.latitude + '////' + pos.coords.longitude);
-      this.set('i', i++);
-    }, { retry: true }, (e) => {
-
-      // alert('onError');
-      alert(e.message + '-----bad');
-      alert('onErrorEnd');
-      this.set('i', i--);
-    });
-      this.set('watchID', watchID);
-  },
-  didInsertElement() {
-    this._super(...arguments);
-  }
-
 });
