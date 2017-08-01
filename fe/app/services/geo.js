4b1206e176b901b274e8c50ed340de90282c309d
Your Name <you@example.com>
2017-08-02 01:59:23 +0800

    x

--- fe/app/services/geo.js
+++ fe/app/services/geo.js
@@ -2,14 +2,26 @@ import Ember from 'ember';
 import { check, f7Alert } from 'fe/utils';
 
 export default Ember.Service.extend({
-  defaultOpts: { timeout: 30000, enableHighAccuracy: true, maximumAge: 0 },
+  defaultOpts: { maximumAge: 3000, timeout: 600000, enableHighAccuracy: true },
   async watch(onSuccess, opts = {}, onError = e => { f7Alert(e.message) }) {
-    await check('deviceready');
+    await check('deviceready').catch(e => {
+      f7Alert(e);
+      throw new Error(e);
+    });
     let defaultOpts = this.get('defaultOpts');
+    if (opts.retry) {
+      onError = () => {
+        alert('retring');
+        navigator.geolocation.watchPosition(onSuccess, onError, { ...defaultOpts, ...opts });
+      }
+    }
     return navigator.geolocation.watchPosition(onSuccess, onError, { ...defaultOpts, ...opts }); // return watchID
   },
   async getCurrentPosition(onSuccess, opts = {}, onError = e => { f7Alert(e.message) }) {
-    await check('deviceready');
+    await check('deviceready').catch(e => {
+      f7Alert(e);
+      throw new Error(e);
+    });
     let defaultOpts = this.get('defaultOpts');
     navigator.geolocation.getCurrentPosition(onSuccess, onError, { ...defaultOpts, ...opts });
   },
--- fe/public/cordova_assets/js/app_init.js
+++ fe/public/cordova_assets/js/app_init.js
@@ -1,5 +1,7 @@
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+  alert('deviceready');
   window.deviceready = true;
 }
