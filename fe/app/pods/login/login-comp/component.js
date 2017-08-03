56e74f66ed1e6a91e2d1b072dc379b912d028076
Your Name <you@example.com>
2017-08-04 00:07:39 +0800

    xxx

--- fe/app/pods/login/login-comp/component.js
+++ fe/app/pods/login/login-comp/component.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement() {
     this._super(...arguments);
+  },
+  actions: {
+    doLogin() {
+      f7App.closeModal('#f7-login-screen');
+    }
   }
 });
